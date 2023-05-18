a5de14eba65b64174fce9195d239144d0cf9e021
Gyula <Gyula@DESKTOP-9M6CS2Q>
2023-05-18 12:50:23 +0200

    Add project files.

---
+++ LINQ_MHegyek/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LINQ_MHegyek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
