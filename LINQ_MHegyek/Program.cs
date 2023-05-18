c7177c11c3963a383f51cf834fe39861c438ed34
Gyula <Gyula>
2023-05-18 14:22:42 +0200

    Feladat

--- LINQ_MHegyek/Program.cs
+++ LINQ_MHegyek/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Hegyek hegyek = new Hegyek("hegyekMo.txt");
+            Console.WriteLine($"3. feladat: {hegyek.GetNev.Count}");
+
+            Console.WriteLine($"4. feladat: {hegyek.GetMagassag.Average()}");
+
+            double legnagyobbHegyMagassaga = 0;
+            int legnagyobbHegyMagasságIndexe = 0;
+            for (int index = 0; index < hegyek.GetMagassag.Count; index++)
+            {
+                if (hegyek.GetMagassag[index] > legnagyobbHegyMagassaga)
+                {
+                    legnagyobbHegyMagassaga = hegyek.GetMagassag[index];
+                    legnagyobbHegyMagasságIndexe = index;
+                }
+            }
+            Console.WriteLine($"5. feladat: {hegyek.GetNev[legnagyobbHegyMagasságIndexe]}, {hegyek.GetHegyseg[legnagyobbHegyMagasságIndexe]}, {hegyek.GetMagassag[legnagyobbHegyMagasságIndexe]}");
+            
+            Console.WriteLine("6. feladat: Kérek egy magasságot: ");
+            double hegymagassagBekeres = Convert.ToDouble(Console.ReadLine());
+            for (int index = 0; index < hegyek.GetHegyseg.Count; index++)
+            {
+                if (hegyek.GetHegyseg[index] == "Börzsöny" && hegyek.GetMagassag[index] > hegymagassagBekeres)
+                {
+                    Console.WriteLine("Van nagyobb!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Nincs {hegymagassagBekeres}-nél nagyobb hegycsúcs!");
+                }
+            }
         }
     }
 }
---
+++ LINQ_MHegyek/hegyek.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_MHegyek
+{
+    internal class Hegyek
+    {
+        List<string> nev = new List<string>();
+        List<string> hegyseg = new List<string>();
+        List<double> magassag = new List<double>();
+        public Hegyek(string fajlnev)
+        {
+            StreamReader streamReader = new StreamReader(fajlnev);
+            streamReader.ReadLine();
+            string sor;
+            while ((sor = streamReader.ReadLine()) != null)
+            {
+                nev.Add(sor.Split(';')[0]);
+                hegyseg.Add(sor.Split(";")[1]);
+                magassag.Add(Convert.ToDouble(sor.Split(";")[2]));
+            }
+            streamReader.Close();
+        }
+
+        public List<string> GetNev { get => nev; }
+        public List<string> GetHegyseg { get => hegyseg; }
+        public List<double> GetMagassag { get => magassag; }
+    }
+}
